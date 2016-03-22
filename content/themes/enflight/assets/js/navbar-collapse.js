c90ae8a7c8e9ff49dc8654081c8c49cf1a4c6ca4
enflightdesign <enflightdesign@gmail.com>
2016-03-21 23:51:39 -0700

    navbar updated

--- content/themes/enflight/assets/js/navbar-collapse.js
+++ content/themes/enflight/assets/js/navbar-collapse.js
@@ -1,75 +1,17 @@
-(function($){
- 
-    $.fn.extend({
-        
-        navResponsive: function() {
- 
-            return this.each(function() {
-                var nav = $(this);
-        
-                // s-collapse menu items
-        
-                nav.find('.nav-menu').find('li a').on('click',function(e){
-                    var ul = $(this).siblings('ul');
-                    if(ul.length){ e.preventDefault(); }
-                    if($(this).closest('nav').hasClass('s-collapse'))
-                    {
-                        ul.find('ul').hide();
-                        ul.find('a .s-collapse-trigger').removeClass('active').text('+');
-                        if(ul.is(':hidden')){ $(this).find('.s-collapse-trigger').addClass('active').text('-'); }
-                        else{ $(this).find('.s-collapse-trigger').removeClass('active').text('+'); }
-                        ul.slideToggle(100);        
-                    }   
-                });
-                
-                /* s-collapse entire menu */
-                
-                nav.find('.s-collapse-trigger').click(function(e){
-                    e.preventDefault();
-                    var nav = $(this).siblings('.nav-menu');
-                    if(nav.is(':hidden')){ $(this).addClass('active').text('-'); }
-                    else{ $(this).removeClass('active').text('+'); } 
-                    $(this).siblings('.nav-menu').slideToggle();
-                });
-                
-                /* set s-collapse based on nav size */
-                
-                var defaultWidth = 0;
-                nav.find('.nav-menu').each(function(){
-                    defaultWidth += $(this).outerWidth();
-                }); 
-                if(nav.innerWidth() < defaultWidth + 150){
-                    nav.addClass('s-collapse'); 
-                    nav.find('.nav-menu').slideUp(500);
-                }
-                
-                $(window).resize(function(){
-                    var el = nav.find('.nav-menu');
-                    if(nav.innerWidth() < defaultWidth + 150)
-                    {
-                        if(!nav.hasClass('s-collapse'))
-                        {
-                            nav.addClass('s-collapse', 300);
-                            el.find('ul').hide();
-                            el.hide();
-                            nav.find('.s-collapse-trigger').removeClass('active').text('+');
-                        }   
-                    }
-                    else
-                    {
-                        if(nav.hasClass('s-collapse'))
-                        {
-                            nav.find('.s-collapse-trigger:last').addClass('active').text('-');      
-                            nav.removeClass('s-collapse', 300);         
-                            el.find('ul').show();
-                            el.fadeIn();                    
-                        }   
-                    }
-                });
-            
-            });
-        }
-    });
-     
-})(jQuery);
+$('a[href^=#]').on('click', function(e){ var href = $(this).attr('href'); $('html, body').animate({ scrollTop:$(href).offset().top },'slow'); e.preventDefault(); });
 
+
+
+/* ----------------------------- 
+Backgroung slider
+----------------------------- */
+$(window).ready(function() {
+    'use strict';
+    $.vegas('slideshow', {
+      backgrounds:[
+        { src:'img/bg-slider/bg-1.jpg', fade:3000 },
+        { src:'img/bg-slider/bg-2.jpg', fade:3000 },
+        { src:'img/bg-slider/bg-3.jpg', fade:3000 }
+      ]
+    })();
+});
