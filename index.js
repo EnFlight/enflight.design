7ec8557c68cf193b0d180dbb032d7422fc4cbb5d
Hon Hoang <Hon@Hons-MacBook-Pro.local>
2016-11-04 00:47:25 +0900

    update

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 // # Ghost Startup
 // Orchestrates the startup of Ghost when run from command line.
-
 var ghost = require('./core'),
+    debug = require('debug')('ghost:boot:index'),
     express = require('express'),
+    logging = require('./core/server/logging'),
     errors = require('./core/server/errors'),
+    utils = require('./core/server/utils'),
     parentApp = express();
 
-// Make sure dependencies are installed and file system permissions are correct.
-require('./core/server/utils/startup-check').check();
-
+debug('Initialising Ghost');
 ghost().then(function (ghostServer) {
     // Mount our Ghost instance on our desired subdirectory path if it exists.
-    parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+    parentApp.use(utils.url.getSubdir(), ghostServer.rootApp);
 
+    debug('Starting Ghost');
     // Let Ghost handle starting our server instance.
     ghostServer.start(parentApp);
 }).catch(function (err) {
-    errors.logErrorAndExit(err, err.context, err.help);
+    logging.error(new errors.GhostError({err: err}));
+    process.exit(0);
 });
